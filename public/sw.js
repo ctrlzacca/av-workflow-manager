3a797a2ab6179dabed8c909f8e3ceaecc2142eed
ctrlzacca <matteo.zcrl@gmail.com>
2026-05-10 12:30:30 +0200

    Fix per android_3

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(clients.claim());
+});
