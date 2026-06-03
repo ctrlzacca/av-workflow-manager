091f901fdef05860826a11a732eed187d2bd7b5b
ctrlzacca <matteo.zcrl@gmail.com>
2026-06-03 18:57:34 +0200

    NEW notifiche_1.2

--- app/lib/pushNotifications.ts
+++ app/lib/pushNotifications.ts
@@ -1,3 +1,13 @@
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+}
+
 export async function subscribeToPush(daysBeore: number): Promise<boolean> {
   if (!("serviceWorker" in navigator) || !("PushManager" in window)) return false;
 
@@ -8,7 +18,9 @@ export async function subscribeToPush(daysBeore: number): Promise<boolean> {
 
   const sub = await reg.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: process.env._VAPID_PUBLIC_KEY!,
+    applicationServerKey: urlBase64ToUint8Array(
+  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!
+)
   });
 
   const res = await fetch("/api/push/subscribe", {
