48e48163fb7f0029f291f742d33cb0016ab020ab
ctrlzacca <matteo.zcrl@gmail.com>
2026-06-03 21:52:23 +0200

    NEW notifiche_2.1

--- app/api/push/test/route.ts
+++ app/api/push/test/route.ts
@@ -21,15 +21,31 @@ console.log("SUBS:", subs);
 
   if (!subs?.length) return Response.json({ error: "no subs" });
 
-  for (const sub of subs) {
-    await webpush.sendNotification(
-      sub.subscription,
-      JSON.stringify({
-        title: "TEST PUSH 🔥",
-        body: "Se vedi questo, tutto funziona",
-      })
-    );
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+for (const sub of subs) {
+  try {
+            await webpush.sendNotification(
+            subs[0].subscription,
+            JSON.stringify({
+                title: "TEST",
+                body: "funziona ancora",
+                url: "/",
+            })
+            );
+  } catch (err) {
+    console.log("Subscription non valida, la elimino:", sub.id);
+
+    await fetch(`${supabaseUrl}/rest/v1/push_subscriptions?id=eq.${sub.id}`, {
+      method: "DELETE",
+      headers: {
+        apikey: supabaseKey,
+        Authorization: `Bearer ${supabaseKey}`,
+      },
+    });
   }
+}
 
   return Response.json({ ok: true });
 }
