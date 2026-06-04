4028f50d41fe51a5d12e0e6aa469646c41e4172d
ctrlzacca <matteo.zcrl@gmail.com>
2026-06-04 15:48:30 +0200

    NEW notifiche_3.5

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,11 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 
 self.addEventListener('push', (e) => {
   const data = e.data?.json() ?? {};
+
   e.waitUntil(
-    self.registration.showNotification(data.title ?? 'AV Workflow', {
+    self.registration.showNotification(data.title ?? 'Notification', {
       body: data.body ?? '',
       icon: '/icon-192.png',
       badge: '/icon-192.png',
